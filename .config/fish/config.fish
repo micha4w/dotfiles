da04415aad14088adb90dfd5eea9b2ddec30bb78
micha4w <micha4w@gmail.com>
2023-06-13 16:06:41 +0200

    fix fish - tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     if not set -q TMUX && [ "$TERM_PROGRAM" != "vscode" ] && [ -z "$VSCODE_RESOLVING_ENVIRONMENT" ]
-        set UNATTACHED_SESSION_ID (tmux ls -F "#{session_name}|#{?session_attached,attached,not attached}" | grep "not attached$" | tail -n 1 | cut -d '|' -f1)
+        set UNATTACHED_SESSION_ID (tmux ls -F "#{session_name}|#{?session_attached,attached,not attached}" | grep "not attached\$" | tail -n 1 | cut -d '|' -f1)
 
         if [ -n "$UNATTACHED_SESSION_ID" ]
             exec tmux attach -t $UNATTACHED_SESSION_ID
@@ -20,9 +20,11 @@ fish_add_path -m /usr/local/go/bin/
 fish_add_path -m ~/.local/bin/
 fish_add_path -m ~/.cargo/bin/
 fish_add_path -m ~/.local/kitty.app/bin/
+fish_add_path -m /opt/gradle/bin/
 
 fish_add_path -m /home/micha4w/.nvm/versions/node/v19.8.1/bin
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias antlr4='java -jar ~/.m2/repository/org/antlr/antlr4/4.12.0/antlr4-4.12.0-complete.jar'
 
 function bind_bang
     switch (commandline --current-token)[-1]
